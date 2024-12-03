4db24ac310d0689a14ee9d80238a3f7971fa6880
100rbhsarin <saurabh007630@gmail.com>
2024-12-03 18:21:32 +0530

    "#" async and await update

--- thisisuseeffect/src/HowNotToFetchApi.jsx
+++ thisisuseeffect/src/HowNotToFetchApi.jsx
@@ -6,21 +6,21 @@ export const HowNotToFetchApi = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const API = "https://pokeapi.co/api/v2/pokemon/charmeleon";
+  const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
 
-    // const fetchPokemon = () => {
-    //   fetch(API)
-    //     .then((res) => res.json())
-    //     .then((data) => {
-    //       setPokemon(data);
-    //       setLoading(false);
-    //     })
-    //     .catch((error) => {
-    //       console.log(error);
-    //       setError(error);
-    //       setLoading(false);
-    //     });
-    // };
+  //   const fetchPokemon = () => {
+  //     fetch(API)
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         setPokemon(data);
+  //         setLoading(false);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //         setError(error);
+  //         setLoading(false);
+  //       });
+  //   };
 
   const fetchPokemon = async () => {
     try {
